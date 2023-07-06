3e213f6094a41be5c1a9261c7d0a412be51813d1
TestProAutomation <oleksandr.panov@testpro.io>
2023-07-06 11:35:50 -0400

    Homework21
    .

---
+++ src/test/java/Homework21.java
@@ -0,0 +1,38 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+
+public class Homework21 extends BaseTest {
+    String newPlaylistName = "Test Pro Edited Playlist2”;
+
+    @Test
+    public void renamePlaylist() {
+        provideEmail("demo@class.com");
+        providePassword("te$t$tudent");
+        clickSubmit();
+        doubleClickPlaylist();
+        enterNewPlaylistName();
+        Assert.assertTrue(doesPlaylistExist());
+    }
+
+    public void doubleClickPlaylist() {
+        WebElement playlistElement = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".playlist:nth-child(3)")));
+        actions.doubleClick(playlistElement);
+    }
+
+    public void enterNewPlaylistName() {
+        WebElement playlistInputField = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("[name='name']")));
+        playlistInputField.sendKeys(Keys.chord(Keys.CONTROL,"A",Keys.BACK_SPACE));
+        playlistInputField.sendKeys(newPlaylistName);
+        playlistInputField.sendKeys(Keys.ENTER);
+    }
+
+    public boolean doesPlaylistExist() {
+        WebElement playlistElement = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("//a[text()='"+newPlaylistName)));
+        return playlistElement.isDisplayed();
+    }
+}
