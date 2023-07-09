3813e720f6f56df81d2ede77a6b6cb2976fd9b3c
TestProAutomation <oleksandr.panov@testpro.io>
2023-07-08 20:24:12 -0400

    Final updates HW16

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -8,22 +8,12 @@ import java.time.Duration;
 
 public class BaseTest {
 
-    public static
 
     @BeforeSuite
     static void setupClass() {
         WebDriverManager.chromedriver().setup();
     }
 
-    @BeforeMethod
-    public void launchBrowser() {
-        // Added ChromeOptions argument below to fix websocket error
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--remote-allow-origins=*");
-
-        driver = new ChromeDriver(options);
-        driver.manager().timeouts().implicitWait(Duration.ofSeconds(10))
-    }
 
 
 }
