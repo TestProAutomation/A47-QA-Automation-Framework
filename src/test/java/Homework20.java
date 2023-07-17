465345288a97f87b650749933b8ffff031f3f74f
TestProAutomation <oleksandr.panov@testpro.io>
2023-07-16 20:37:26 -0400

    HW20 Final updates

--- src/test/java/Homework20.java
+++ src/test/java/Homework20.java
@@ -1,44 +1,44 @@
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import java.time.Instant;
-import java.time.temporal.Temporal;
-import java.time.temporal.TemporalUnit;
 
 public class Homework20 extends BaseTest {
-    String newPlaylistName = "Test Pro Edited Playlist";
-
     @Test
-    public void renamePlaylist() {
+    public void deletePlaylist() throws InterruptedException {
+        String deletedPlaylistMsg = "Deleted playlist";
         provideEmail("demo@class.com");
         providePassword("te$t$tudent");
         clickSubmit();
-        doubleClickPlaylist();
-        enterNewPlaylistName();
-        Assert.assertTrue(doesPlaylistExist());
+        openPlaylist();
+        clickDeletePlaylistBtn();
+        confirmDelete();
+
+        Assert.assertTrue(getDeletePlaylistMsg().contains(deletedPlaylistMsg));
     }
 
-    public void doubleClickPlaylist() {
-        WebElement playlistElement = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".playlist:nth-child(3)")));
-        actions.doubleClick(playlistElement);
+    public void openPlaylist() {
+        WebElement emptyPlaylist = driver.findElement(By.cssSelector(".playlist:nth-child(3)"));
+        emptyPlaylist.click();
     }
 
-    public void enterNewPlaylistName() {
-        WebElement playlistInputField = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("[name='name']")));
-        playlistInputField.sendKeys(Keys.chord(Keys.CONTROL,"A",Keys.BACK_SPACE));
-        playlistInputField.sendKeys(newPlaylistName);
-        playlistInputField.sendKeys(Keys.ENTER);
+    public void clickDeletePlaylistBtn() throws InterruptedException {
+        WebElement deletePlaylist = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".btn-delete-playlist")));
+       // WebElement deletePlaylist = driver.findElement(By.cssSelector(".btn-delete-playlist"));
+        deletePlaylist.click();
+       // Thread.sleep(2000);
     }
 
-    public boolean doesPlaylistExist() {
-        WebElement playlistElement = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("//a[text()='"+newPlaylistName)));
-        return playlistElement.isDisplayed();
+    public void confirmDelete() {
+        WebElement confirmBtn = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("button.ok")));
+        // WebElement confirmBtn = driver.findElement(By.cssSelector("button.ok"));
+        confirmBtn.click();
+    }
+    public String getDeletePlaylistMsg() {
+        WebElement notificationMsg = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("div.success.show")));
+       // WebElement notificationMsg = driver.findElement(By.cssSelector("div.success.show"));
+        return notificationMsg.getText();
     }
 }
-
-
-
